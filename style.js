c22ee8101200ed1e9d18f9c65fa57d7aaabc98e5
Madina <madinarazavi19@gmail.com>
2026-09-11 20:32:16 +0430

    Add about page files

---
+++ style.js
@@ -0,0 +1,17 @@
+const form = document.querySelector("form");
+
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    if (username ==="admin" && password ==="12345678") {
+        alert("login successful!");
+        window.location.href="home.html";
+    } else {
+        alert("incorrect username or password");
+        console.log("Username:", username);
+        console.log("Password:", password);
+    }
+});
